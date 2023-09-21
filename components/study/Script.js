8caf63dde98cb45d342e22312c3d41e945229c0a
Alon Landmann <rafael.landmann@gmail.com>
2023-09-21 08:24:41 +0800

    add rules

--- components/study/Script.js
+++ components/study/Script.js
@@ -8,6 +8,7 @@ import Definition from '../units/Definition'
 import Theorem from '../units/Theorem'
 import Example from '../units/Example'
 import Exercise from '../units/Exercise'
+import Rule from '../units/Rule'
 
 export default function Script({ sectionContent }) {
   return (
@@ -22,6 +23,7 @@ export default function Script({ sectionContent }) {
           {unit.type === 'notion' && <Notion unit={unit} />}
           {unit.type === 'definition' && <Definition unit={unit} />}
           {unit.type === 'theorem' && <Theorem unit={unit} />}
+          {unit.type === 'rule' && <Rule unit={unit} />}
           {unit.type === 'example' && <Example unit={unit} />}
           {unit.type === 'exercise' && <Exercise unit={unit} />}
         </div>
