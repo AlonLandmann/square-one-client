4d8f98cc56cb3f5b38492eec4448ca8120e7511a
Alon Landmann <rafael.landmann@gmail.com>
2023-09-21 11:12:05 +0800

    previous and next in navbar

--- components/home/HomeRoot.js
+++ components/home/HomeRoot.js
@@ -4,7 +4,7 @@ import css from '@/scss/home/HomeRoot.module.scss'
 export default function HomeRoot() {
   return (
     <div>
-      <Link href='/study/logical-foundations'>Logical Foundations</Link>
+      <Link href='/logical-foundations/1/1'>Logical Foundations</Link>
     </div>
   )
 }
--- components/study/Breadcrumbs.js
+++ components/study/Breadcrumbs.js
@@ -1,6 +1,21 @@
 import css from '@/scss/study/Breadcrumbs.module.scss'
+import { useRouter } from 'next/router'
 
 export default function Breadcrumbs({ sectionContent }) {
+  const router = useRouter()
+
+  function navToPrevious() {
+    if (sectionContent.previous) {
+      router.push(sectionContent.previous)
+    }
+  }
+
+  function navToNext() {
+    if (sectionContent.next) {
+      router.push(sectionContent.next)
+    }
+  }
+
   return (
     <div className={css.container}>
       <div className={css.logo}>
@@ -18,6 +33,14 @@ export default function Breadcrumbs({ sectionContent }) {
       <div><i className='bi bi-slash-lg'></i></div>
       <div className={css.section}>
         {sectionContent.section.name}
+      </div>
+      <div className={css.navs}>
+        <div onClick={navToPrevious}>
+          <i className='bi bi-chevron-left'></i>
+        </div>
+        <div onClick={navToNext}>
+          <i className='bi bi-chevron-right'></i>
+        </div>
       </div>
     </div>
   )
