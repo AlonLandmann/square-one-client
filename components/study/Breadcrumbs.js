19068b1f38e01246932fb96d6d28bcd5478eac44
Alon Landmann <rafael.landmann@gmail.com>
2023-09-22 11:21:55 +0800

    reset breadcrumbs

--- components/study/Breadcrumbs.js
+++ components/study/Breadcrumbs.js
@@ -1,21 +1,6 @@
 import css from '@/scss/study/Breadcrumbs.module.scss'
-import { useRouter } from 'next/router'
 
 export default function Breadcrumbs({ sectionContent }) {
-  const router = useRouter()
-
-  function navToPrevious() {
-    if (sectionContent.previous) {
-      router.push(sectionContent.previous)
-    }
-  }
-
-  function navToNext() {
-    if (sectionContent.next) {
-      router.push(sectionContent.next)
-    }
-  }
-
   return (
     <div className={css.container}>
       <div className={css.logo}>
@@ -28,23 +13,14 @@ export default function Breadcrumbs({ sectionContent }) {
       </div>
       <div><i className='bi bi-slash-lg'></i></div>
       <div className={css.chapter}>
+        {sectionContent.chapter.nr}{' '}
         {sectionContent.chapter.name}
       </div>
       <div><i className='bi bi-slash-lg'></i></div>
       <div className={css.section}>
+        {sectionContent.chapter.nr}.
+        {sectionContent.section.nr}{' '}
         {sectionContent.section.name}
-      </div>
-      <div className={css.navs}>
-        <div onClick={navToPrevious}>
-          {sectionContent.previous &&
-            <i className='bi bi-chevron-left'></i>
-          }
-        </div>
-        <div onClick={navToNext}>
-          {sectionContent.next &&
-            <i className='bi bi-chevron-right'></i>
-          }
-        </div>
       </div>
     </div>
   )
