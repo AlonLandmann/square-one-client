187d205d76852f70a1349fa6f48c3da15023b42d
Alon Landmann <rafael.landmann@gmail.com>
2023-09-24 12:19:53 +0800

    add stack

---
+++ components/study/Stack.js
@@ -0,0 +1,30 @@
+import css from '@/scss/study/Stack.module.scss'
+import { v4 as uuid } from 'uuid'
+import Text from '../units/Text'
+import Notion from '../units/Notion'
+import Definition from '../units/Definition'
+import Axiom from '../units/Axiom'
+import Theorem from '../units/Theorem'
+import Example from '../units/Example'
+import Exercise from '../units/Exercise'
+import Rule from '../units/Rule'
+
+
+export default function Stack({ stack, setStack }) {
+  return (
+    <div className={css.container}>
+      {stack.map(unit => (
+        <div key={uuid()}>
+          {unit.type === 'text' && <Text unit={unit} />}
+          {unit.type === 'notion' && <Notion unit={unit} />}
+          {unit.type === 'definition' && <Definition unit={unit} />}
+          {unit.type === 'axiom' && <Axiom unit={unit} />}
+          {unit.type === 'theorem' && <Theorem unit={unit} />}
+          {unit.type === 'rule' && <Rule unit={unit} />}
+          {unit.type === 'example' && <Example unit={unit} />}
+          {unit.type === 'exercise' && <Exercise unit={unit} />}
+        </div>
+      ))}
+    </div>
+  )
+}
--- components/study/StudyRoot.js
+++ components/study/StudyRoot.js
@@ -3,23 +3,30 @@ import Navbar from './Navbar'
 import Script from './Script'
 import getSectionContent from '@/lib/getSectionContent'
 import { useState } from 'react'
+import ModuleProvider from '@/lib/ModuleProvider'
+import StackProvider from '@/lib/StackProvider'
+import Stack from './Stack'
 
 export default function StudyRoot({ module, modulesInfo, chapterNr, sectionNr }) {
   const sectionContent = getSectionContent(module, chapterNr, sectionNr)
   const [dd, setDd] = useState(null)
+  const [stack, setStack] = useState([])
 
   return (
-    <div>
-      <Navbar
-        module={module}
-        modulesInfo={modulesInfo}
-        sectionContent={sectionContent}
-        dd={dd}
-        setDd={setDd}
-      />
-      <div className={css.main} onMouseEnter={() => { setDd(null) }}>
-        <Script sectionContent={sectionContent} />
-      </div>
-    </div>
+    <ModuleProvider value={module}>
+      <StackProvider value={[stack, setStack]}>
+        <Navbar
+          module={module}
+          modulesInfo={modulesInfo}
+          sectionContent={sectionContent}
+          dd={dd}
+          setDd={setDd}
+        />
+        <div className={css.main} onMouseEnter={() => { setDd(null) }}>
+          <Script sectionContent={sectionContent} />
+          <Stack stack={stack} setStack={setStack} />
+        </div>
+      </StackProvider>
+    </ModuleProvider>
   )
 }
