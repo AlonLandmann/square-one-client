03517f46f429171d9084c83aada20c9b7d0d3fde
Alon Landmann <rafael.landmann@gmail.com>
2023-09-24 12:31:29 +0800

    add remove from stack

--- components/study/Stack.js
+++ components/study/Stack.js
@@ -11,18 +11,27 @@ import Rule from '../units/Rule'
 
 
 export default function Stack({ stack, setStack }) {
+  function removeFromStack(index) {
+    setStack(prev => prev.filter(unit => unit.index !== index))
+  }
+
   return (
     <div className={css.container}>
       {stack.map(unit => (
-        <div key={uuid()}>
-          {unit.type === 'text' && <Text unit={unit} />}
-          {unit.type === 'notion' && <Notion unit={unit} />}
-          {unit.type === 'definition' && <Definition unit={unit} />}
-          {unit.type === 'axiom' && <Axiom unit={unit} />}
-          {unit.type === 'theorem' && <Theorem unit={unit} />}
-          {unit.type === 'rule' && <Rule unit={unit} />}
-          {unit.type === 'example' && <Example unit={unit} />}
-          {unit.type === 'exercise' && <Exercise unit={unit} />}
+        <div key={uuid()} className={css.stackItem}>
+          <div className={css.unit}>
+            {unit.type === 'text' && <Text unit={unit} />}
+            {unit.type === 'notion' && <Notion unit={unit} />}
+            {unit.type === 'definition' && <Definition unit={unit} />}
+            {unit.type === 'axiom' && <Axiom unit={unit} />}
+            {unit.type === 'theorem' && <Theorem unit={unit} />}
+            {unit.type === 'rule' && <Rule unit={unit} />}
+            {unit.type === 'example' && <Example unit={unit} />}
+            {unit.type === 'exercise' && <Exercise unit={unit} />}
+          </div>
+          <div className={css.removeButton} onClick={() => { removeFromStack(unit.index) }}>
+            <i className='bi bi-trash3'></i>
+          </div>
         </div>
       ))}
     </div>
