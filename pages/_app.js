99519062960114603c8c6321be8d8651c28d745c
Alon Landmann <rafael.landmann@gmail.com>
2023-09-25 11:01:46 +0800

    add toaster

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Toaster } from 'react-hot-toast'
 import '@/scss/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  )
 }
