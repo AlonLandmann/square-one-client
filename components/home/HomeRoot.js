beac16edd6bbb7dca8cccf2d4c3c0d4efb4db539
Alon Landmann <rafael.landmann@gmail.com>
2023-09-25 14:17:25 +0800

    add hero section

--- components/home/HomeRoot.js
+++ components/home/HomeRoot.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Navbar from '@/components/home/Navbar'
 import useAuth from '@/hooks/useAuth'
 import css from '@/scss/home/HomeRoot.module.scss'
@@ -12,6 +13,21 @@ export default function HomeRoot() {
   return (
     <div>
       <Navbar user={user} />
+      <section className={css.heroSection}>
+        <div className={css.heroImage}>
+          <Image
+            src='/hero.jpg'
+            alt='styud-image'
+            fill
+            objectFit='cover'
+            priority
+          />
+        </div>
+        <div className={css.heroCard}>
+          <h1>Welcome to Square One</h1>
+          <h3>We study math, science, and technology the right way - from the ground up.</h3>
+        </div>
+      </section>
     </div>
   )
 }
