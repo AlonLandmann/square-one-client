0734d4fe0d0cacafa9754dd6092eca81493e8d5f
Alon Landmann <rafael.landmann@gmail.com>
2023-09-26 12:48:36 +0800

    adjust css

--- components/info/Description.js
+++ components/info/Description.js
@@ -1,9 +1,14 @@
 import css from '@/scss/info/Description.module.scss'
 
-export default function Description() {
+export default function Description({ module }) {
   return (
     <div className={css.container}>
-      
+      <div className={css.content}>
+        <div className={css.heading}>Description</div>
+        <div className={css.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Ut lectus arcu bibendum at varius vel pharetra vel. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Ut morbi tincidunt augue interdum velit euismod in. Felis eget velit aliquet sagittis id consectetur purus ut. Rhoncus aenean vel elit scelerisque mauris pellentesque. Sem et tortor consequat id porta nibh venenatis cras sed. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Et malesuada fames ac turpis egestas sed tempus urna et. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Ultricies lacus sed turpis tincidunt. At augue eget arcu dictum varius duis at. Ultrices vitae auctor eu augue ut lectus arcu. Cursus in hac habitasse platea dictumst.
+        </div>
+      </div>
     </div>
   )
 }
